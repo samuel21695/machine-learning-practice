8c41727c8ee5791679635847bcf8bd568c95474f
samuel21695 <samuel21695@gmail.com>
2024-02-20 13:01:41 +0900

    ✨ model training and evaluation

---
+++ app.py
@@ -0,0 +1,21 @@
+# 라이브러리 import
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+# 데이터 불러오기
+iris_df = pd.read_csv("iris.csv")
+
+# 데이터 분할
+X_train, X_test, y_train, y_test = train_test_split(iris_df.drop("iris_species", axis=1), iris_df["iris_species"], test_size=0.2)
+
+# 모델 생성 및 학습
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train, y_train)
+
+# 예측 수행
+y_pred = model.predict(X_test)
+
+# 정확도 평가
+accuracy = model.score(X_test, y_test)
+print("정확도:", accuracy)
